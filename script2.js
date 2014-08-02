498ef88cf4c7cdcacd2239e1ea71c332c2a0d83a
Nick-Mazuk <trainridernick@gmail.com>
2014-08-02 15:58:23 -0700

    Added second homepage

---
+++ script2.js
@@ -0,0 +1,11 @@
+var onload = function() {
+
+	function setImg(){
+		var numRand = Math.floor(Math.random()*(2)) + 1;
+		document.getElementById("addimage").src = "Pictures/Sponsors/"+numRand+".png";
+
+	}
+
+	setInterval(setImg(),500);
+
+};
